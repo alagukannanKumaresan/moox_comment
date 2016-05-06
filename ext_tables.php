b905cdc8890a5fbc039d6748b7483767c5682bc7
Jakub Czyz <info@jakub-czyz.de>
2016-05-06 15:24:25 +0200

    update icons

--- ext_tables.php
+++ ext_tables.php
@@ -42,12 +42,12 @@ if (TYPO3_MODE === 'BE') {
         $TBE_MODULES = $temp_TBE_MODULES;
 		if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('moox_core')) {
 			$mainModuleKey 		= "DCNGmbH.moox_core";
-			$mainModuleIcon 	= 'EXT:moox_core/ext_icon32.png';
-			$mainModuleLabels 	= 'LLL:EXT:moox_core/Resources/Private/Language/MainModule.xlf';			
+			$mainModuleIcon 	= 'EXT:moox_core/Resources/Public/Icons/module-mooxcore-white.svg';
+			$mainModuleLabels 	= 'LLL:EXT:moox_core/Resources/Private/Language/Module.xlf:moox_main_module_tab';
 		} else {
 			$mainModuleKey 		= "DCNGmbH.".$_EXTKEY;
-			$mainModuleIcon 	= 'EXT:'.$_EXTKEY.'/Resources/Public/Moox/MainModuleExtIcon.png';
-			$mainModuleLabels 	= 'LLL:EXT:'.$_EXTKEY.'/Resources/Public/Moox/MainModule.xlf';
+			$mainModuleIcon 	= 'EXT:'.$_EXTKEY.'/Resources/Public/Moox/module-mooxcore-white.svg';
+			$mainModuleLabels 	= 'LLL:EXT:'.$_EXTKEY.'/Resources/Public/Moox/Module.xlf:moox_main_module_tab';
 		}
 		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
 			$mainModuleKey,
@@ -76,7 +76,7 @@ if (TYPO3_MODE === 'BE') {
 		),
 		array(
 			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/moox_comment_icon.svg',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_init.xlf',
 		)
 	);
@@ -103,6 +103,6 @@ if (TYPO3_MODE == 'BE') {
 }
 
 // Icon in page tree
-$TCA['pages']['columns']['module']['config']['items'][] = array('MOOX-Comments', 'mxcomment', 'EXT:moox_comment/ext_icon.gif');
-\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon('pages', 'contains-mxcomment', '../typo3conf/ext/moox_comment/ext_icon.gif');
+$TCA['pages']['columns']['module']['config']['items'][] = array('MOOX-Comments', 'mxcomment', 'EXT:moox_comment/Resources/Public/Icons/moox_comment_icon.svg');
+\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon('pages', 'contains-mxcomment', '../typo3conf/ext/moox_comment/Resources/Public/Icons/moox_comment_icon.svg');
 ?>
