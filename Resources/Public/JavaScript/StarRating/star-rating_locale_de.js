3e669a087a1fa5f43c22232d00cf86022de3373c
Jakub Czyz <info@jakub-czyz.de>
2016-05-06 13:49:38 +0200

    compatibility updates

---
+++ Resources/Public/JavaScript/StarRating/star-rating_locale_de.js
@@ -0,0 +1,41 @@
+/*!
+ * Star Rating German Translations
+ *
+ * This file must be loaded after 'star-rating.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * @see http://github.com/kartik-v/bootstrap-star-rating
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+(function ($) {
+    "use strict";
+    $.fn.ratingLocales['de'] = {
+        defaultCaption: '{rating} Sterne',
+        starCaptions: {
+            0.5: 'Halber Stern',
+            1: 'Ein Stern',
+            1.5: 'Eineinhalb Sterne',
+            2: 'Zwei Sterne',
+            2.5: 'Zweieinhalb Sterne',
+            3: 'Drei Sterne',
+            3.5: 'Dreieinhalb Sterne',
+            4: 'Vier Sterne',
+            4.5: 'Viereinhalb Sterne',
+            5: 'Fünf Sterne',
+            5.5: 'Fünfeinhalb Sterne',
+            6: 'Sechs Sterne',
+            6.5: 'Sechseinhalb Sterne',
+            7: 'Sieben Sterne',
+            7.5: 'Siebeneinhalb Sterne',
+            8: 'Acht Sterne',
+            8.5: 'Achteinhalb Sterne',
+            9: 'Neun Sterne',
+            9.5: 'Neuneinhalb Sterne',
+            10: 'Zehn Sterne'
+        },
+        clearButtonTitle: 'Zurücksetzen',
+        clearCaption: 'Nicht Bewertet'
+    };
+})(window.jQuery);
--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2.0-7.9.99',
-			'moox_core' => '0.9.9-1.9.99'
+			'moox_core' => '0.9.9-7.9.99'
 		),
 		'conflicts' => array(
 		),
